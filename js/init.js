b07f078d1032818c4a52c8efc971b84750399ee5
khamiltonuk <krisham87@hotmail.com>
2014-02-28 07:09:48 +0000

    leaving the pub with things animating

--- js/init.js
+++ js/init.js
@@ -4,8 +4,8 @@
 
         var settings = $.extend({
             'element': $(this),
-                'duration': 500,
-                'direction': 'up'
+            'duration': 500,
+            'direction': 'up'//I probably would extend this an test if tool tip was in the viewport
         }, options);
         popbox = function ($el) {
             $el.show();
@@ -15,7 +15,7 @@
             //console.log(settings.duration);
         },
         init = function () {
-            box = settings.element.find('.pop-up');
+            var box = settings.element.find('.pop-up');
             settings.element.hover(function () {
                 popbox(box);
             }, function () {
@@ -27,97 +27,102 @@
 
 })(jQuery);
 
+//Carousel 
 (function ($) {
     $.fn.slideCarousel = function (options) {
-        var rebecca = function () {
-            var count = 0,
-                $carousel_elements = $('#main-image').find('li'),
-                item_width = $carousel_elements.outerWidth(),
-                distance = 0,
-                amount_slides = $carousel_elements.size(),
-                slides = ($carousel_elements.size() - 1),
-                inner_width = amount_slides * item_width;
-            //console.log(item_width);
+        var settings = $.extend({
+            'element': $(this),
+            'animationDuration': 500,
+            'direction': 'up'
+        }, options);
 
+        var count = 0,
+            $carousel_elements = settings.element.find('li'),
+            item_width = $('#frame').outerWidth(),
+            distance = 0,
+            amount_slides = $carousel_elements.length,
+            slides = $carousel_elements.length - 1,
+            inner_width = amount_slides * item_width,
+            thumbnails_links = $('#thumbnails li a');
 
-            var rebeccasPicks = function () {
-                function movement(count) {
-                    $('#prev, #next').removeClass('disabled');
-                    $carousel_elements.removeClass('current');
-                    var distance = item_width * count,
-                        indie_bar_distance = ((count - 1) * 36 + 3);
-                    $('#main-image').animate({
-                        'left': -distance
-                    });
-                    $carousel_elements.eq(count).addClass('current');
-
-                    if (count === 0) {
-                        $('#prev').addClass('disabled');
-                    }
-                    if (count == slides) {
-                        $('#next').addClass('disabled');
-                    }
-                }
-
-                $$('#main-image').css({
-                    'width': inner_width
+        function movement(count,animate) {
+            $('#prev, #next').removeClass('disabled');
+            $carousel_elements.removeClass('current');
+            thumbnails_links.removeClass('current');
+            var distance = item_width * count,
+                indie_bar_distance = ((count - 1) * 36 + 3);
+            if (animate === false){
+                settings.element.css({
+                    'left': -distance
                 });
+            }else{
+                settings.element.animate({
+                    'left': -distance
+                }, settings.animationDuration );
+            }
+            
+            $carousel_elements.eq(count).addClass('current');
+            $('#thumbnails li a').eq(count).addClass('current');
 
-                $('#next').click(function () {
-                    if (count >= 0 && count < slides) {
-                        movement(++count);
-                    }
-                });
+            if (count === 0) {
+                $('#prev').addClass('disabled');
+            }
+            if (count == slides) {
+                $('#next').addClass('disabled');
+            }
+        }
 
-                $('#prev').click(function () {
-                    if (count <= slides && count >= 1) {
-                        movement(--count);
-                    }
-                });
-                console.log('3');
-            };
-        };
-        return rebecca();
+        var mainImage = document.getElementById('main-image');
+        mainImage.style.width = inner_width + 'px';
+
+        $('.arrow').on('click', function (event) {
+            if (event.target.id == "next" && count >= 0 && count < slides) {
+                movement(++count);
+            }
+            if (event.target.id == "prev" && count <= slides && count >= 1) {
+                movement(--count);
+            }
+            event.preventDefault();
+        });
+        
+        $('#thumbnails').on('click', 'a', function (e) {
+            var thumbsIndex = $(this).parent().index();
+            movement(thumbsIndex, false);
+            e.preventDefault();
+        });  
     };
 })(jQuery);
 
 
-
+//getting the images from flickr
 (function ($) {
     $.fn.searchFlickr = function (options) {
-        var url = 'http://api.flickr.com/services/rest/';
-        var gallery = $(this);
+        var url = 'http://api.flickr.com/services/rest/',
+            gallery = $(this);
 
         var settings = $.extend({
-            'api_key': '6591bac6b104953cb9e6944a5b65b509',
-                'defaultSearchTerm': 'dad'
+            'api_key': '6eada3f33715e9c7005d6e278e98cb2c'
         }, options);
 
         getSearchValue = function () {
             $('#search-submit').click(function (event) {
-                searchValue = $('#search').val();
+                var searchValue = $('#search').val();
                 $('#what-you-want').html(searchValue);
                 getImages(searchValue);
                 event.preventDefault();
             });
         },
-        updateMainimage = function (imageSrc) {
-            newimage = '<li class="active"><img src="' + image_src + '" />';
-            $('#main-image').append(newimage);
-            $('#main-image').find('.active').siblings().remove();
-        },
-        updateThumbnailsCurrent = function (newThumb) {
-
-        },
         getImages = function (searchTerm) {
+            
+        $('#message').html('Please wait...').fadeIn();
             $.getJSON(url, {
                 method: 'flickr.photos.search',
                 api_key: settings.api_key,
                 media: 'photos',
-                tags: 'dad', //searchTerm,
+                tags: searchTerm,
                 per_page: 15,
                 format: 'json',
-                extras: 'url_q,url_l',
+                extras: 'url_q,url_l,owner_name,',
                 nojsoncallback: 1
             }).success(function (state) {
                 var list = $('#thumbnails ul'),
@@ -125,38 +130,36 @@
                 list.html('');
                 viewport.html(' ');
                 $.each(state.photos.photo, function () {
-                    //(this.isprimary == "1") {
-                    viewport.append('<li><img src="' + this.url_l + '" /></li>'); //<div class="image-info">' + this.title + '<br />' + this.tags + '</div>');
+                    viewport.append('<li><img src="' + this.url_l + '" alt="' + this.title + '" data-ownername="' + this.ownername + '"/></li>'); 
                     list.append('<li><a href="' + this.url_l + '"><img src="' + this.url_q + '" ' + 'data-title="' + this.title + '" ' + 'data-url="' + this.url_l + '" /></a></li>');
-
                 });
+                $('#message').fadeOut().html(' ');
+
+                $('#main-image').slideCarousel();
+
                 list.find('li:first-child a').addClass('current');
-                viewport.find('li:first-child a').addClass('current');
-                //gallery.children('h3').html(state.photoset.description._content);
+                var firstImage = viewport.find('li:first-child');
+                
+                firstImage.addClass('current');
+                setOwnerText(firstImage);
             }).fail(function (state) {
-                alert("unable to retrieve photo set information");
+                $('#message').html('oops something has gone wrong').fadeIn();
             });
         },
-        thumbnailsEventHandler = function () {
-            $('#thumbnails').on('click', 'a', function (event) {
-                $('#thumbnails li a').removeClass('current');
-                event.preventDefault();
-                $(this).addClass('current');
-                image_src = $(this).attr('href');
-            });
-        },
-        nextimage = function () {
-            $('#next').on('click', function (e) {
-                next_image = $('#thumbnails li a').hasClass('.current'); //.parent().next().find('a').attr('href');
-                alert(next_image);
-            });
+        setOwnerText = function(image){
+            var ownerName = image.find('img').data("ownername");
+            if (ownerName){
+                $('#owner-name').html(ownerName);
+            }else {
+                $('#owner-name').html('Unknown');
+            }
+            $('#description').fadeIn();
         },
         init = function () {
-            thumbnailsEventHandler();
+            oname = document.getElementById('description');
+            oname.style.display = 'none';
             getSearchValue();
-            //updateMainimage();
-            //console.log(settings.defaultSearchTerm);
-            getImages();
+            getImages('mum');
         };
         return init();
 
@@ -164,8 +167,7 @@
 
 })(jQuery);
 
-$(document).on('ready', function () {
+document.addEventListener('DOMContentLoaded', function(){
     $('.js-share').shareTooltip();
-    $('#main-image').slideCarousel();
     $('div#thumbnails').searchFlickr();
 });
